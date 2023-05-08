1e5e82b5b784b11007519fa65eee9803107b2004
Nicolás Rapp <nicolas.rapp.ar@gmail.com>
2023-05-08 00:19:21 -0300

    Minor Changes

--- Solucion-TP-FERRETERIA/Clases/Main.cpp
+++ Solucion-TP-FERRETERIA/Clases/Main.cpp
@@ -13,10 +13,21 @@ using namespace std;
 
 int main() 
 {
-	Articulo* Art1 = new Articulo(567, true, "Perfecto", "clavo", 2.05, 3.04, 4.01, 34);
+	Articulo Art1 = Articulo(567, true, "Perfecto", "clavo", 2.05, 3.04, 4.01, 34);
 
 	vector <Articulo> Articulos;
+	vector <Articulo>::iterator arr;
+
+	int i = 0;
+
+	for (arr = Articulos.begin(); arr != Articulos.end(); arr++, i++)
+	{
+		Articulos[i] = Art1;
+		
+	}
 
 	Cliente* Javier = new Cliente("Javier", "Peña", "45545166", Otro, "Manuel Ugarte 5500", Efectivo, Articulos ,"Foto", "ArtRoto", true);
+
+	return 0;
  
 }
